e7be1687be0a8102eace49896a513999ef7a4e9a
annisamonitha <annisamonitha12@gmail.com>
2018-05-14 02:12:11 +0700

    navipb

--- application/views/account/beranda.php
+++ application/views/account/beranda.php
@@ -43,7 +43,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
               <a class="nav-link" href="#"><?php echo anchor('homeguest','Home');?></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#"><?php echo anchor('login','Sign In');?></a>
+              <a class="nav-link" href="#"><?php echo anchor('login','Log In');?></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#"><?php echo anchor('register','Sign Up');?></a>
