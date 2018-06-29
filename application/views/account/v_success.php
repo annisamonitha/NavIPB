54c7a61d9c93136d8990514465eb9cfaa07369d8
aulia_5117 <manaufaladam@gmail.com>
2018-06-29 23:55:38 +0700

    a
    a

--- application/views/account/v_success.php
+++ application/views/account/v_success.php
@@ -1,194 +1,54 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
+ <?php
+ defined('BASEPATH') OR exit('No direct script access allowed');
+ ?><!DOCTYPE html>
+ <head>
+   <meta charset="UTF-8">
+    <title>
+      Notification | NavIPB
+    </title>
 
-<!DOCTYPE html>
-<html lang="en">
+    <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ;?>">
 
-  <head>
+   <script src="<?php echo base_url('assets/jquery/jquery-3.3.1.min.js') ;?>"></script>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+   <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js') ;?>"></script>
 
-    <title>Welcome to NavIPB!</title>
+   <script src="<?php echo base_url('assets/datatables/js/jquery.dataTables.min.js') ;?>"></script>
 
-    <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url('assets/beranda/vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
+   <script src="<?php echo base_url('assets/datatables/js/dataTables.bootstrap.js') ;?>"></script>
 
-    <!-- Custom fonts for this template -->
-    <link href="<?php echo base_url('assets/beranda/vendor/font-awesome/css/font-awesome.min.css')?>" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+ </head>
+ <body>
 
-    <!-- Custom styles for this template -->
-    <link href="<?php echo base_url('assets/beranda/css/clean-blog.min.css')?>" rel="stylesheet">
+   <p><?php echo $message; ?></p>
+    <p> Silahkan <?php echo anchor('Login','Login'); ?></p>
+ </body>
+ </html>
 
-  </head>
+<style>
+ body {
+   margin: 0;
+   padding: 0;
+   width: 100%;
+   position: absolute;
+   background-image: url('http://icanbecreative.com/resources/files/articles/40-high-resolution-wallpapers-for-minimalist-lovers/sources/everest-minimalist-wallpaper-blue.png');
+   background-size: cover;
+ }
 
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand" href="#">NavIPB</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i class="fa fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#"><?php echo anchor('homeguest','Maps');?></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><?php echo anchor('login','Log In');?></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><?php echo anchor('register','Sign Up');?></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    <!-- Page Header -->
-    <header class="masthead" style="background-image: url('<?php echo base_url('assets/beranda/img/foto_beranda.png')?>')">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 col-md-10 mx-auto">
-            <div class="site-heading">
-              <h1>NavIPB</h1>
-              <span class="subheading">Navigation for IPB Citizen</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-
- <h5> 
-	<div class="notice success">
-  	 <p><?php echo $message; ?></p>
-</div>
-</h5>
-
-    <!-- Footer -->
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 col-md-10 mx-auto">
-            <ul class="list-inline text-center">
-              <li class="list-inline-item">
-                <a href="https://www.facebook.com/BogorAgriculturalUniversity/">
-                  <span class="fa-stack fa-lg">
-                    <i class="fa fa-circle fa-stack-2x"></i>
-                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                  </span>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://www.instagram.com/official_ipb/">
-                  <span class="fa-stack fa-lg">
-                    <i class="fa fa-circle fa-stack-2x"></i>
-                    <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
-                  </span>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://twitter.com/ipbofficial/">
-                  <span class="fa-stack fa-lg">
-                    <i class="fa fa-circle fa-stack-2x"></i>
-                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                  </span>
-                </a>
-              </li>
-            </ul>
-            <p class="copyright text-muted">Copyright &copy; Ilmu Komputer IPB 2018</p>
-          </div>
-        </div>
-      </div>
-    </footer>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="<?php echo base_url('assets/beranda/vendor/jquery/jquery.min.js')?>"></script>
-    <script src="<?php echo base_url('assets/beranda/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
-
-    <!-- Custom scripts for this template -->
-    <script src="<?php echo base_url('assets/beranda/js/clean-blog.min.js')?>"></script>
-
-  </body>
-
- <style>
-  h3 {
-  font-family: 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif;
-  color: #34495e;
-  -webkit-font-smoothing: antialiased;
-  line-height: 1.6em;
-}
-
-h3 {
-  margin: 0;
-}
-
-.notice {
-  position: relative;
-  margin: 1em;
-  background: #eae5e5;
-  padding: 1em 1em 1em 2em;
-  border-left: 4px solid #DDD;
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.125);
-}
-
-.notice:before {
-  position: absolute;
-  top: 50%;
-  margin-top: -17px;
-  left: -17px;
-  background-color: #eae5e5;
-  color: #FFF;
-  width: 20px;
-  height: 20px;
-  border-radius: 100%;
-  text-align: center;
-  line-height: 30px;
-  font-weight: bold;
-  font-family: Georgia;
-  text-shadow: 1px 1px rgba(0, 0, 0, 0.5);
-}
-
-.success {
-  border-color: #2ECC40;
-}
-
-.success:before {
-  content: "√";
-  background-color: #2ECC40;
-}
-
-</style>
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+ p {
+     width: 400px;
+     height: 580px;
+     background: black;
+     padding: 60px 30px;
+     position: fixed;
+     left: 50%;
+     top: 55%;
+     transform: translate(-50%, -50%);
+     border-style:solid;
+     border-color: white;
+     border-width: 3px;
+     border-width: 3px;
+     background:rgba(12,12,70,0.8);
+     color: #fff;
+     border-radius: 20px;
+     }
